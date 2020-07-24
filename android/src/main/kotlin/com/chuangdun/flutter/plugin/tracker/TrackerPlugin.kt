b040eb52cb18ede9bc413926773696168881dedb
WangLiang <wl@lookmap.net>
2020-07-24 18:25:24 +0800

    android插件方法定义

--- android/src/main/kotlin/com/chuangdun/flutter/plugin/tracker/TrackerPlugin.kt
+++ android/src/main/kotlin/com/chuangdun/flutter/plugin/tracker/TrackerPlugin.kt
@@ -1,6 +1,13 @@
 package com.chuangdun.flutter.plugin.tracker
 
+import android.Manifest
+import android.app.Activity
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
@@ -9,17 +16,19 @@ import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 import io.flutter.plugin.common.PluginRegistry.Registrar
 
+const val METHOD_CHANNEL_NAME = "com.chuangdun.flutter/tracker/methods"
+const val EVENT_CHANNEL_NAME = "com.chuangdun.flutter/tracker/events"
+
+
 /** TrackerPlugin */
-public class TrackerPlugin: FlutterPlugin, MethodCallHandler {
-  /// The MethodChannel that will the communication between Flutter and native Android
-  ///
-  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-  /// when the Flutter Engine is detached from the Activity
-  private lateinit var channel : MethodChannel
+public class TrackerPlugin: FlutterPlugin, MethodCallHandler{
+  private lateinit var methodChannel : MethodChannel
+  private lateinit var context: Context
+  private lateinit var activity: Activity
 
   override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-    channel = MethodChannel(flutterPluginBinding.getFlutterEngine().getDartExecutor(), "tracker")
-    channel.setMethodCallHandler(this);
+    methodChannel = MethodChannel(flutterPluginBinding.getFlutterEngine().getDartExecutor(), METHOD_CHANNEL_NAME)
+    methodChannel.setMethodCallHandler(this)
   }
 
   // This static function is optional and equivalent to onAttachedToEngine. It supports the old
@@ -34,20 +43,38 @@ public class TrackerPlugin: FlutterPlugin, MethodCallHandler {
   companion object {
     @JvmStatic
     fun registerWith(registrar: Registrar) {
-      val channel = MethodChannel(registrar.messenger(), "tracker")
-      channel.setMethodCallHandler(TrackerPlugin())
+      val trackerPlugin = TrackerPlugin()
+      trackerPlugin.context = registrar.context()
+
+      val channel = MethodChannel(registrar.messenger(), METHOD_CHANNEL_NAME)
+      channel.setMethodCallHandler(trackerPlugin)
     }
   }
 
   override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
-    if (call.method == "getPlatformVersion") {
-      result.success("Android ${android.os.Build.VERSION.RELEASE}")
-    } else {
-      result.notImplemented()
+    when(call.method){
+      "start" -> {
+        val permissions = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q){
+          arrayOf(Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION,
+                  Manifest.permission.ACCESS_BACKGROUND_LOCATION)
+        }else{
+          arrayOf(Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION)
+        }
+        val notGrantedPermissions = permissions.filterNot {
+          ActivityCompat.checkSelfPermission(context,it) == PackageManager.PERMISSION_GRANTED}
+        if (notGrantedPermissions.isNotEmpty()){
+          ActivityCompat.requestPermissions(activity, notGrantedPermissions.toTypedArray(), 9999)
+        }
+      }
+      "stop" -> {
+
+      }
+      else -> result.notImplemented()
     }
   }
 
   override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
-    channel.setMethodCallHandler(null)
+    methodChannel.setMethodCallHandler(null)
   }
+
 }
