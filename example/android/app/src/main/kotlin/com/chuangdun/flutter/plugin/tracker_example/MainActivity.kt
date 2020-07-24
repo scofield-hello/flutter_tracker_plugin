9514bf0247d0d360d23c42e3cac43513b0944ba0
WangLiang <wl@lookmap.net>
2020-07-24 15:41:38 +0800

    初始化提交

---
+++ example/android/app/src/main/kotlin/com/chuangdun/flutter/plugin/tracker_example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.chuangdun.flutter.plugin.tracker_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ ios/Classes/TrackerPlugin.m
@@ -0,0 +1,20 @@
+#import "TrackerPlugin.h"
+
+@implementation TrackerPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"tracker"
+            binaryMessenger:[registrar messenger]];
+  TrackerPlugin* instance = [[TrackerPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
